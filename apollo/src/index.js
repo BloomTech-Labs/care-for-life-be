9c8bab8d18d888ba6a80e463f42c333ef07e42ff
Bernie Durfee <berniedurfee@users.noreply.github.com>
2020-04-08 10:35:23 -0400

    Prismatopia updates

--- apollo/src/index.js
+++ apollo/src/index.js
@@ -4,11 +4,6 @@
 const { importSchema } = require('graphql-import');
 const { ApolloServer, gql } = require('apollo-server');
 
-// Import resolvers and context
-const resolvers = require('./resolvers');
-const context = require('./context');
-
-// Declare port
 const PORT = process.env.PORT || 8000;
 
 const checkEnvironment = () => {
@@ -27,26 +22,32 @@ const checkEnvironment = () => {
   }
 }
 
-async function main() {
-  const typeDefs = await importSchema('schema/schema.graphql');
+const resolvers = require('./resolvers');
+const context = require('./context');
+
+const typeDefs = gql(importSchema('schema/schema.graphql'));
+
+(async () => {
+  // Check the environment
+  checkEnvironment()
 
   const server = new ApolloServer({
     resolvers,
-    typeDefs: gql(typeDefs),
+    typeDefs,
     context,
-    cors: {
-      origin: '*', // <- allow request from all domains
-      credentials: true,
+    cors: true,
+    formatError: err => {
+      // Don't give the specific errors to the client.
+      console.log('%O', err);
+      console.log('%O', err.extensions);
+
+      // Otherwise return the original error.  The error can also
+      // be manipulated in other ways, so long as it's returned.
+      return err;
     },
-    dataSources: () => ({}),
   });
 
-  server.listen({ port: PORT }).then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
-}
-
-// Check the environment
-checkEnvironment()
-
-main();
+  const { url } = await server.listen(PORT);
+  // eslint-disable-next-line no-console
+  console.log(`=========Running on ${url}=========`);
+})();
