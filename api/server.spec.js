31f8ac1cc3c334b5be609352972503cf6e4a2c43
Brandon <teaguebrandon@outlook.com>
2020-05-27 13:43:32 -0400

    wrote some tests for surveys

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,22 +2,6 @@ const request = require('supertest');
 const db = require('../data/db-config');
 const server = require('./server');
 
-beforeEach((done) => {
-    db.migrate.rollback().then(() => {
-        db.migrate.latest().then(() => {
-            return db.seed.run().then(() => {
-                done();
-            });
-        });
-    });
-});
-
-afterEach((done) => {
-    db.migrate.rollback().then(() => {
-        done();
-    });
-});
-
 describe("server.js", () => {
     it("should return a status of 200", async () => {
         let res = await request(server).get("/");
