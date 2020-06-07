64a769a5ef0dd9de1c9c933176fcdcf525ad47b6
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-06-07 14:57:29 -0700

    add comments to auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -8,14 +8,17 @@ router.get('/login', (req, res) => {
     axios.post(`https://dev-815303.okta.com/oauth2/default/v1/introspect?client_id=0oadb0iolJz1QUG2c4x6&token=${token}&token_type_hint=id_token`)
         .then(response => {
             console.log(response.data)
+            // check db for email address
             db.login(response.data.username)
                 .then((user) => {
+                    // check if user is returned
                     if (user.length > 1) {
                         res.status(200).json({
                             isRegistered: true,
                             name: response.data.name,
                             email: response.data.username
                         })
+                        // if array is empty
                     } else {
                         res.status(200).json({
                             isRegistered: false,
@@ -24,7 +27,7 @@ router.get('/login', (req, res) => {
                         })
                     }
                 })
-                .catch(err => res.status(500).json(err.message))
+                .catch(err => res.status(400).json(err.message))
         })
         .catch(err => res.status(500).json(err.message))
 })
