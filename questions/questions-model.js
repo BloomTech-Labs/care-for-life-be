a1f6f8f2c85960c80c1a68420c2d5f00145c9df6
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-06-16 12:39:09 -0700

    update question model

--- questions/questions-model.js
+++ questions/questions-model.js
@@ -13,7 +13,7 @@ async function find() {
     return db('question as q')
         .join('survey as s', 's.id', 'q.survey_id')
         .join('question_type as qt', 'qt.id', 'q.question_type')
-        .select("q.id", "q.question", 'q.question_type', 'qt.type')
+        .select("q.id", "q.question", 'q.question_type', 'q.survey_id', 'qt.type')
     // const id = db('users').select('id').orderBy('id')
     // const arrId = id.map(id => { return id.id });
     // return await arrId.map(id => findUserDetails(id))
