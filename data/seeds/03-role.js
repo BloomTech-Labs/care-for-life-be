bf084896d4d8e5ab0d9b823de8d410707999d769
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-06-17 17:06:37 -0700

    remove promoter from role list

--- data/seeds/03-role.js
+++ data/seeds/03-role.js
@@ -4,7 +4,6 @@ exports.seed = function (knex) {
     { role: 'supervisor_agriculture' },
     { role: 'supervisor_health' },
     { role: 'supervisor_income_generation' },
-    { role: 'promoter' },
     { role: 'teacher' },
     { role: 'field manager' },
     { role: 'field officer' }
